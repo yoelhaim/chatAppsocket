91edc3dae853cf518186d2ab8fd4de25e001a4c6
youssefelhaimer8 <youssefelhaimer8@gmail.com>
2021-05-07 04:10:03 +0000

    update in public folder

--- route/chatRoute.js
+++ route/chatRoute.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const routes = express.Router();
 const { addchat, getchat } = require("../controllers/user.controler");
+const { addRelChat, getRel } = require("../controllers/relChatModels");
 
-routes.post("/add", addchat);///////
-routes.get("/chat", getchat);//////
+routes.post("/add", addchat); ///////
+routes.get("/chat/:rec/:send", getchat); //////
+routes.post("/addRel", addRelChat);
+routes.get("/getRel/:senderId/:receverId", getRel);
 
 module.exports = routes;

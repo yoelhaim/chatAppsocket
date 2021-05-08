da289a043064d804d6019cc66913f8c45cf808ab
youssefelhaimer8 <youssefelhaimer8@gmail.com>
2021-05-08 14:03:25 +0000

    change  succese chat

--- controllers/user.controler.js
+++ controllers/user.controler.js
@@ -1,4 +1,4 @@
-const { chat, users } = require("../model");
+const { chat, users, relcaht } = require("../model");
 const today = require("../config/date");
 const httperror = require("http-errors");
 const { Op } = require("sequelize");
@@ -22,7 +22,6 @@ module.exports = {
   },
 
   getchat: async (req, res, next) => {
-    console.log(req.user);
     try {
       if (parseInt(req.params.send) != req.user.id) {
         throw httperror.Forbidden("not permission user");
@@ -41,5 +40,61 @@ module.exports = {
       next(error);
     }
   },
+
+  getchatHead: async (req, res, next) => {
+    try {
+      // if (parseInt(req.params.send) != req.user.id) {
+      //   throw httperror.Forbidden("not permission user");
+      // }
+      const chats = await chat.findAll({
+        order: [["time", "DESC"]],
+
+        where: {
+          [Op.or]: [{ senderId: req.params.id }, { receverId: req.params.id }],
+          receverId: { [Op.ne]: req.params.id },
+        },
+        // attributes: ['receverId'],
+        group: ["receverId"],
+        include: [
+          {
+            // Notice `include` takes an ARRAY
+            model: users,
+          },
+        ],
+      });
+      res.send(chats);
+    } catch (error) {
+      // next(error);
+    }
+  },
   /// all data
+
+  updateLast: async (req, res, next) => {
+    try {
+      // if (parseInt(req.params.send) != req.user.id) {
+      //   throw httperror.Forbidden("not permission user");
+      // }
+      await relcaht
+        .update(
+          {
+            lastmsg: req.body.lastmsg,
+            time: req.body.time,
+          },
+          {
+            where: {
+              senderId: req.body.senderId,
+              receverId: req.body.receverId,
+            },
+          }
+        )
+        .then((resu) => {
+          res.send(resu);
+        })
+        .catch((err) => {
+          res.send(err.message);
+        });
+    } catch (error) {
+      // next(error);
+    }
+  },
 };

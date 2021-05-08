637003b0b6c66b43f38222934fa0b7946efdb1d6
youssefelhaimer8 <youssefelhaimer8@gmail.com>
2021-05-08 14:04:04 +0000

    login

--- route/authRoute.js
+++ route/authRoute.js
@@ -3,9 +3,11 @@ const {
   createAccount,
   login,
   getUser,
+  
 } = require("../controllers/auth.controler");
 
 routes.post("/create", createAccount);
 routes.post("/login", login);
 routes.get("/user/:id", getUser);
+
 module.exports = routes;
